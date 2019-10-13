d8c43c0f589b105b7c019edd9069eed22d0628dc
David Villegas Aguilar <david.villegas.aguilar@gmail.com>
2019-10-13 17:41:37 -0400

    Arreglado error de registro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
 
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
-            $table->string('role')->default('pacient');  // 'admin', 'pacient', 'doctor'
+            $table->string('role')->default('patient');  // 'admin', 'pacient', 'doctor'
 
 
             $table->rememberToken();
